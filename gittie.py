3086b67156688c4476e54f7fe809256ba322e623
crabvendor <st.wyrobek@gmail.com>
2018-05-23 10:48:45 +0200

    added joke function

--- gittie.py
+++ gittie.py
@@ -1,2 +1,17 @@
 def introduce():
     print("Hello, I'm Gittie!")
+
+
+def joke():
+    print("""Telling a joke:
+           
+                 ROFL:ROFL:ROFL:ROFL
+                     _^___
+             L    __/   [] |
+             LOL===__        |
+             L      \________]
+                     I   I
+                     --------/""")
+
+
+joke()
